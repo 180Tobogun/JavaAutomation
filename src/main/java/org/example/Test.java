2a8634caacfe8466585724df056ce77b95154d1d
180tobogun <andreykoroka@gmail.com>
2023-01-04 15:22:12 +0200

    add driver

--- src/main/java/org/example/Test.java
+++ src/main/java/org/example/Test.java
@@ -1,7 +1,11 @@
 package org.example;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
 
 public class Test {
     public static void main(String[] args) {
@@ -9,6 +13,29 @@ public class Test {
 
         WebDriver driver = new ChromeDriver();
 
-        driver.get("https://www.google.com/");
+        driver.get("https://djinni.co/my/dashboard/");
+
+        WebElement loginEmail = driver.findElement(By.id("email"));
+        loginEmail.sendKeys();
+
+        WebElement webelement = driver.findElement(By.xpath("//li[@class = 'nav-item active']"));
+
+        webelement.click();
+
+        WebElement findProf = driver.findElement(By.xpath("//*[contains (text(), 'QA Automation')]"));
+
+        findProf.click();
+
+        List<WebElement> checkSalary = driver.findElements(By.xpath(
+                "//div[@class = 'summary-stastistics--item summary-stastistics--item_vacancies']" +
+                " //div[@class = 'summary-item']" +
+                " [2] " +
+                "//div[@class = 'summary-item--value']"));
+
+        if (checkSalary.size() == 1) {
+            System.out.println("element present");
+        } else {
+            System.out.println("cant find");
+        }
     }
 }
